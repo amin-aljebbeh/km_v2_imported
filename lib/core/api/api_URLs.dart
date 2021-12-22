615cc883e078e6b98b7eec932a1e4fc60dba60b2
Amin <amin.aljebbeh@gmail.com>
2021-12-22 09:21:12 +0200

    change base_url

--- lib/core/api/api_URLs.dart
+++ lib/core/api/api_URLs.dart
@@ -3,10 +3,10 @@ const String TEST_URL = "http://test.kammun.com";
 const String APP_URL = "https://kammun.app";
 
 // ignore: non_constant_identifier_names
-String BaseUrl = TEST_URL;
+String BaseUrl = APP_URL;
 
 const String APPLE_BASE_URL = "https://kammun.app";
-const String PRODUCTION_BASE_URL = TEST_URL;
+const String PRODUCTION_BASE_URL = APP_URL;
 
 const String GET_COMPANY = "/api/company";
 const String GET_MOBILE_VERSION = "/api/mob_v_config";
