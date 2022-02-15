6f777239fca5b95f26b5691fa968c68cb8642d5e
Mohammad Rabia Najib <forgitrabia@gmail.com>
2022-02-15 18:46:10 +0200

    change letter

--- lib/views/widget/order_detail_view_main_card.dart
+++ lib/views/widget/order_detail_view_main_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kammun_app/Services.dart';
 import 'package:kammun_app/models/models_importer.dart';
-import 'package:kammun_app/views/Widget/widgets_importer.dart';
+import 'package:kammun_app/views/widget/widgets_importer.dart';
 import 'package:kammun_app/views/loading/LoadingServices.dart';
 import 'package:kammun_app/views/order_details/full_screen_image.dart';
 import 'package:kammun_app/utils/utils_importer.dart';
