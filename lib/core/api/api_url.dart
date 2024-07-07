903e336cb2d5f0fc2edf42cbe3ae2912b16c7285
rabie <mhmd.rabie.njib@gmail.com>
2024-07-07 22:13:57 +0400

    [IMP] [Code] Init branch

--- lib/core/api/api_url.dart
+++ lib/core/api/api_url.dart
@@ -1,5 +1,5 @@
-const String testUrl = 'http://apptest.kammun.com';
-const String appUrl = 'https://kammun.app';
+const String testUrl = 'http://betaapi.kammun.com';
+const String appUrl = 'http://betaapi.kammun.com';
 const String user = 'user/';
 const String searchApi = 'search/';
 const String supportedCityApi = 'supported_city/';
