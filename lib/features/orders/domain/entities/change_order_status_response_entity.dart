c1556a331509ab4d4cd7bde18bc0b24383b8ded8
rabie <mhmd.rabie.njib@gmail.com>
2024-10-09 20:51:40 +0400

    [IMP] [Orders] Remove order from "changeOrderStatus" response

--- lib/features/orders/domain/entities/change_order_status_response_entity.dart
+++ lib/features/orders/domain/entities/change_order_status_response_entity.dart
@@ -1,9 +1,6 @@
-import 'package:kammun_app/features/orders/domain/entities/order_entity.dart';
-
 class ChangeOrderStatusResponseEntity {
   final bool success;
   final String data;
-  final OrderEntity order;
 
-  ChangeOrderStatusResponseEntity({this.success, this.data, this.order});
+  ChangeOrderStatusResponseEntity({this.success, this.data});
 }
