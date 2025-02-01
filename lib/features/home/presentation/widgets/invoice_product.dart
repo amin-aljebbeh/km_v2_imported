33a0ffe8a2fd894712b1d19e5345c83aa8c1145c
rabieNajib <mhmd.rbie.njib@gmail.com>
2025-02-01 15:01:04 +0400

    [FIX] [Special Product] Open the product page

--- lib/features/home/presentation/widgets/invoice_product.dart
+++ lib/features/home/presentation/widgets/invoice_product.dart
@@ -17,38 +17,35 @@ class InvoiceProduct extends StatelessWidget {
       builder: (context, state) {
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: InkWell(
-            onTap: () => Navigator.push(
+          child: KCard(onTap: () => Navigator.push(
                 context, MaterialPageRoute(builder: (context) => ProductDetailView(product: product))),
-            child: KCard(
-              radius: const BorderRadius.all(Radius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      KCacheImage(
-                          tag: product.productId,
-                          image: product.images.isNotEmpty ? product.images[0].imageFileName : ''),
-                      Text(
-                        product.name.split(' ').length <= 2
-                            ? product.name
-                            : product.name.split(' ')[0] +
-                            ' ' +
-                            (product.name.split(' ')[1] ?? ' ') +
-                            '...',
-                        textAlign: TextAlign.center,
-                        style: textFieldStyle,
-                      ),
-                      Text(
-                          product.unit.toString() != 'null'
-                              ? product.quantity.split('.')[0] + ' ' + product.unit
-                              : product.quantity.split('.')[0],
-                          style: invoiceProductStyle),
-                    ],
-                  ),
+            radius: const BorderRadius.all(Radius.circular(20)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    KCacheImage(
+                        tag: product.productId,
+                        image: product.images.isNotEmpty ? product.images[0].imageFileName : ''),
+                    Text(
+                      product.name.split(' ').length <= 2
+                          ? product.name
+                          : product.name.split(' ')[0] +
+                          ' ' +
+                          (product.name.split(' ')[1] ?? ' ') +
+                          '...',
+                      textAlign: TextAlign.center,
+                      style: textFieldStyle,
+                    ),
+                    Text(
+                        product.unit.toString() != 'null'
+                            ? product.quantity.split('.')[0] + ' ' + product.unit
+                            : product.quantity.split('.')[0],
+                        style: invoiceProductStyle),
+                  ],
                 ),
               ),
             ),
