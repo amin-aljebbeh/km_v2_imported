f1ebb5368cd8aa47373cbdc72b42f2fd05bfff4f
rabieNajib <mhmd.rbie.njib@gmail.com>
2025-02-03 19:16:33 +0400

    [FIX] [Special Product] View price

--- lib/features/home/presentation/widgets/invoice_product.dart
+++ lib/features/home/presentation/widgets/invoice_product.dart
@@ -17,8 +17,9 @@ class InvoiceProduct extends StatelessWidget {
       builder: (context, state) {
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: KCard(onTap: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => ProductDetailView(product: product))),
+          child: KCard(
+            onTap: () =>
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductDetailView(product: product))),
             radius: const BorderRadius.all(Radius.circular(20)),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -33,13 +34,29 @@ class InvoiceProduct extends StatelessWidget {
                     Text(
                       product.name.split(' ').length <= 2
                           ? product.name
-                          : product.name.split(' ')[0] +
-                          ' ' +
-                          (product.name.split(' ')[1] ?? ' ') +
-                          '...',
+                          : product.name.split(' ')[0] + ' ' + (product.name.split(' ')[1] ?? ' ') + '...',
                       textAlign: TextAlign.center,
                       style: textFieldStyle,
                     ),
+                    Text(
+                      StringUtils().oCcy.format(int.parse(product.price.split('.')[0]) + (product.discountValue ?? 0)) +
+                          ' ' +
+                          state.generalInformationState.companyInformation.currency,
+                      textAlign: TextAlign.right,
+                      style: invoiceProductStyle.copyWith(
+                          decoration:
+                              ((product.discountValue ?? 0) != 0) ? TextDecoration.lineThrough : TextDecoration.none,
+                          decorationThickness: 25),
+                    ),
+                    if (product.discountValue != null)
+                      if (product.discountValue != 0)
+                        Text(
+                          StringUtils().oCcy.format(int.parse(product.price.split('.')[0])) +
+                              ' ' +
+                              state.generalInformationState.companyInformation.currency,
+                          textAlign: TextAlign.right,
+                          style: invoiceProductStyle.copyWith(color: kmRed),
+                        ),
                     Text(
                         product.unit.toString() != 'null'
                             ? product.quantity.split('.')[0] + ' ' + product.unit
